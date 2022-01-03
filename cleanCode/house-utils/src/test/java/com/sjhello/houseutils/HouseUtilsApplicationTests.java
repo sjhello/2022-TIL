fc4fb353a78169773a3819540febe05083221a37
sjhello <sjhello235@gmail.com>
2022-01-04 01:46:25 +0900

    클린코드 1장, 2장 init

---
+++ cleanCode/house-utils/src/test/java/com/sjhello/houseutils/HouseUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjhello.houseutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
