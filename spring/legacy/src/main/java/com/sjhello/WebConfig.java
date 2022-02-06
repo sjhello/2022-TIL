19b8919c01b0901155a4fa6756fc3095e2ad476c
sjhello <sjhello235@gmail.com>
2022-02-06 18:27:34 +0900

    spring: WebMvcConfigurer를 사용하여 viewResolver를 설정하기

--- spring/legacy/src/main/java/com/sjhello/WebConfig.java
+++ spring/legacy/src/main/java/com/sjhello/WebConfig.java
@@ -1,28 +1,19 @@
 package com.sjhello;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.HandlerAdapter;
-import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer{
 
-	@Bean
-	public ViewResolver viewResolver() {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setPrefix("/WEB-INF/view/");
-		viewResolver.setSuffix(".jsp");
-		return viewResolver;
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/view/", ".jsp");
 	}
 }
