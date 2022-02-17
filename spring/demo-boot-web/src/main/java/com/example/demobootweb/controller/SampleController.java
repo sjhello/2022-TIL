25d02a3fd09b769dcb6c171f1ad60afbf7fc0f94
sjhello <sjhello235@gmail.com>
2022-02-17 21:58:17 +0900

    spring: 잭슨 라이브러리로 메시지 컨버터 테스트

--- spring/demo-boot-web/src/main/java/com/example/demobootweb/controller/SampleController.java
+++ spring/demo-boot-web/src/main/java/com/example/demobootweb/controller/SampleController.java
@@ -2,6 +2,7 @@ package com.example.demobootweb.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,15 @@ public class SampleController {
 	@GetMapping("/helloId")
 	public String helloRequestParamId(@RequestParam("id") Product product) {
 		return "hello " + product.getName();
+	}
+
+	@GetMapping("/message")
+	public String stringMessage(@RequestBody String message) {
+		return message;
+	}
+
+	@GetMapping(value = "/jsonMessage")
+	public Person jsonMessage(@RequestBody Person jsonMessage) {
+		return jsonMessage;
 	}
 }
--- spring/demo-boot-web/src/test/java/com/example/demobootweb/controller/SampleControllerTest.java
+++ spring/demo-boot-web/src/test/java/com/example/demobootweb/controller/SampleControllerTest.java
@@ -10,10 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.example.demobootweb.domain.Person;
 import com.example.demobootweb.domain.Product;
 import com.example.demobootweb.repository.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,6 +27,9 @@ class SampleControllerTest {
 
 	@Autowired
 	ProductRepository productRepository;
+
+	@Autowired
+	ObjectMapper objectMapper;
 
 	@Test
 	void hello() throws Exception {
@@ -67,5 +73,31 @@ class SampleControllerTest {
 			.andExpect(status().isOk())
 			.andExpect(content().string(Matchers.containsString("Hello Mobile")))
 			.andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
+	}
+
+	@Test
+	void stringMessage() throws Exception {
+		this.mockMvc.perform(get("/message")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("hello"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(content().string("hello"));
+	}
+
+	@Test
+	void jsonMessage() throws Exception {
+		Person person = new Person();
+		person.setId(1L);
+		person.setName("sjhello");
+
+		this.mockMvc.perform(get("/jsonMessage")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(person)))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.id").value("1"))
+			.andExpect(jsonPath("$.name").value("sjhello"));
 	}
 }
