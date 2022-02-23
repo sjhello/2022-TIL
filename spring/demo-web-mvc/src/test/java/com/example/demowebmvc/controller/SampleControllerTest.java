a312315a0444d87e6f92b663d742fe65c4c778a0
sjhello <sjhello235@gmail.com>
2022-02-24 00:06:38 +0900

    spring: OPTIONS 테스트

--- spring/demo-web-mvc/src/test/java/com/example/demowebmvc/controller/SampleControllerTest.java
+++ spring/demo-web-mvc/src/test/java/com/example/demowebmvc/controller/SampleControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.demowebmvc.controller;
 
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -7,6 +8,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
@@ -21,5 +23,21 @@ class SampleControllerTest {
 			.andDo(print())
 			.andExpect(status().isOk())
 			.andExpect(content().string("sample"));
+	}
+
+	@Test
+	void sampleOptions() throws Exception {
+		mockMvc.perform(options("/sample"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(header().stringValues(HttpHeaders.ALLOW, hasItems(containsString("POST"))));
+	}
+
+	@Test
+	void sampleOptionsFail() throws Exception {
+		mockMvc.perform(options("/sample"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(header().stringValues(HttpHeaders.ALLOW, "GET","POST"));
 	}
 }
