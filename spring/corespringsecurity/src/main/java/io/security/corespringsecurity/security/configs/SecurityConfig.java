91afe94c75f6c9c45fc81108d9d37af322fe6b8f
sjhello <sjhello235@gmail.com>
2022-03-01 22:06:12 +0900

    spring: 권한에 따른 security 설정 적용

--- spring/corespringsecurity/src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ spring/corespringsecurity/src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,19 +1,45 @@
 package io.security.corespringsecurity.security.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private String plainText = "1234";
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		String password = passwordEncoder().encode(plainText);
+
+		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
+		auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+			.antMatchers("/").permitAll()
+			.antMatchers("/messages").hasRole("MANAGER")
+			.antMatchers("/mypage").hasRole("USER")
+			.antMatchers("/config").hasRole("ADMIN")
 			.anyRequest().authenticated()
 		.and()
 			.formLogin();
 	}
+
+
 }
