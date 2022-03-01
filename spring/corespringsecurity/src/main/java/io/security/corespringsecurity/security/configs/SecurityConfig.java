6f89677e50753b35e415fc7336c4671ae8111ae0
sjhello <sjhello235@gmail.com>
2022-03-01 22:14:52 +0900

    spring: 상위 권한이 하위 권한을 갖도록 수정

--- spring/corespringsecurity/src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ spring/corespringsecurity/src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,9 +1,11 @@
 package io.security.corespringsecurity.security.configs;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -19,9 +21,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		String password = passwordEncoder().encode(plainText);
 
-		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
-		auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
 		auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
+		auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
+		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN","USER","MANAGER");
 	}
 
 	@Bean
@@ -33,13 +35,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers("/").permitAll()
-			.antMatchers("/messages").hasRole("MANAGER")
-			.antMatchers("/mypage").hasRole("USER")
-			.antMatchers("/config").hasRole("ADMIN")
+			.antMatchers("/mypage").hasAnyRole("USER", "MANAGER", "ADMIN")
+			.antMatchers("/messages").hasAnyRole("MANAGER", "ADMIN")
+			.antMatchers("/config").hasAnyRole("ADMIN")
 			.anyRequest().authenticated()
 		.and()
 			.formLogin();
 	}
-
-
 }
